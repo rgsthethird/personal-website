d27b49466ec7be932caca89a0ea956df49e85ba1
Rob Schwartz <rgsthethird@gmail.com>
2022-06-06 10:06:51 -0700

    Optimized site

--- app.js
+++ app.js
@@ -8,10 +8,6 @@ app.get('/', function(req, res){
 	res.render("home");
 });
 
-app.get('/undertheumbrella', function(req, res){
-	res.render("blog");
-});
-
 const port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, function(){
 	console.log("Running server...");
