e61a812c9ef7192a46b0ab4d9fb3d4b905dcc8cf
Rob Schwartz <rgsthethird@gmail.com>
2022-07-30 21:05:06 -0700

    Added ebay endpoint

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+const { createHash } = require('crypto');
 
 var app = express();
 app.set("view engine", "ejs");
@@ -7,6 +8,20 @@ app.use(express.static('public'));
 app.get('/', function(req, res){
 	res.render("home");
 });
+
+app.post('/ebay', (req, res) => {
+	const challengeCode = req.query.challenge_code;
+	const verificationToken = 'asdfjhawenljk32h234asdfjkh_asdfaks2jvnme3';
+	const endpoint = 'www.robschwartz.co/ebay'
+	const hash = createHash('sha256');
+	hash.update(challengeCode);
+	hash.update(verificationToken);
+	hash.update(endpoint);
+	const responseHash = hash.digest('hex');
+	const challengeResponse = new Buffer.from(responseHash).toString();
+	res.setHeader('content-type', 'application/json');
+	res.send({challengeResponse})
+})
 
 const port = process.env.PORT || 3000;
 app.listen(port, process.env.IP, function(){
